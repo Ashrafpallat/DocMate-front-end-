2dd4f3f2d3ccfb328c3d9c6277778b493bc7b487
Ashrafpallat <ashrafpallat123@gmail.com>
2025-01-10 21:01:19 +0530

    change tailwind config module.export to export default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind to scan these files for class names
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: '#4A4A4A',   // Example for dark gray
-//         secondary: '#A3A3A3', // Example for light gray
-//         accent: '#C2B8A3',    // Example for warm taupe
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind to scan these files for class names
   ],
